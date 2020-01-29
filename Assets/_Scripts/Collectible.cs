b7b36f485b0bf8a24941f223d779f0332c0d02ca
Dangonok <dangonok@gmail.com>
2020-01-29 22:10:31 +0100

    fps collectible & post process

---
+++ Assets/_Scripts/Collectible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.collectibles.Add(this);
+        GameManager.Instance.SetSonar();
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.Instance.collectibles.Remove(this);
+    }
+}
---
+++ Assets/_Scripts/OndeCollectible.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class OndeCollectible : MonoBehaviour
+{
+    [SerializeField] SimpleSonarShader_Object simpleShader;
+    Sequence sequence;
+    IEnumerator timer;
+
+    float lastTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Light());
+        //simpleShader.StartSonarRing(transform.position, 1)
+        //sequence = DOTween.Sequence();
+        //sequence.AppendInterval(0.5f).OnComplete(() => simpleShader.StartSonarRing(transform.position, 1));
+        //sequence.Restart();
+    }
+
+    private IEnumerator Light()
+    {
+        lastTime = Time.time;
+        for (; ; )
+        {
+            if(lastTime + 0.5f < Time.time)
+            {
+                lastTime = Time.time;
+                simpleShader.StartSonarRing(transform.position, 1);
+                print("LIGHT");
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
